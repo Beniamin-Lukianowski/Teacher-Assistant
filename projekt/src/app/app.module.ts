f1d56aeec20a844bc9c064190ac5a6d4f1047740
Beniamin <Beniamin>
2022-10-09 16:20:42 +0200

    zmiany

--- projekt/src/app/app.module.ts
+++ projekt/src/app/app.module.ts
@@ -8,6 +8,13 @@ import {Auth, getAuth, User } from '@angular/fire/auth';
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AuthService } from "./shared/services/auth.service";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { welcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
@@ -23,11 +30,17 @@ import { environment } from 'src/environments/environment';
     
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     BrowserModule,  
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
+    
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {   }
